373fb862fe2b54b37a2f48383fa81420c9917d99
emas-paul <pjobiggi@gmail.com>
2020-05-01 23:27:35 +0100

    Advert  page completed

--- assets/js/aos-master/init.js
+++ assets/js/aos-master/init.js
@@ -17,7 +17,7 @@ AOS.init({
     delay: 50, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
-    once: true, // whether animation should happen only once - while scrolling down
+    once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom',
     disable: 'mobile' // defines which position of the element regarding to window should trigger the animation
